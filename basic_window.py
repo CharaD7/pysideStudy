c3ce5e52ee666ed599c04dd52c770ab920692a32
CharaD7 <jijakahn6@gmail.com>
2024-07-25 23:02:58 +0000

    Created basic button in Window

---
+++ basic_window.py
@@ -0,0 +1,23 @@
+import sys
+
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import ( QApplication, QMainWindow, QPushButton )
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press me!")
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(button)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
