4d1831945dee168736347ed5d4b22312b1ea1af3
CharaD7 <jijakahn6@gmail.com>
2024-07-26 11:41:11 +0000

    Stored buttton states in a variable

--- basic_window.py
+++ basic_window.py
@@ -8,23 +8,24 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.button_is_checked = True
+
         self.setWindowTitle("My App")
 
         button = QPushButton("Press me!")
         button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
         button.clicked.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)
 
         self.setFixedSize(QSize(400, 300))
         # Set the central widget of the Window. Widget will expand
         # to take up all the space in the window by default.
         self.setCentralWidget(button)
 
-    def the_button_was_clicked(self):
-        print("Clicked!")
-
     def the_button_was_toggled(self, checked):
-        print("Checked?", checked)
+        self.button_is_checked = checked
+
+        print(self.button_is_checked)
 
 app = QApplication(sys.argv)
 
