d178c4bede61526276c87ebca2fe0ed67ea8cccb
CharaD7 <jijakahn6@gmail.com>
2024-07-26 10:59:17 +0000

    Added signal

--- basic_window.py
+++ basic_window.py
@@ -6,16 +6,21 @@ from PySide6.QtWidgets import (QApplication, QMainWindow, QPushButton)
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+        super().__init__()
 
         self.setWindowTitle("My App")
 
         button = QPushButton("Press me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
 
         self.setFixedSize(QSize(400, 300))
         # Set the central widget of the Window. Widget will expand
         # to take up all the space in the window by default.
         self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
 
 app = QApplication(sys.argv)
 
